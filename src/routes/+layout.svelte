e07faaee3c0c9ac1e4dd59b798422a60a26c364b
Francisco Cunha <francisccdn@gmail.com>
2025-01-03 22:34:11 +0100

    Add header with navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,45 @@
 <script lang="ts">
 	import '../app.css';
+	import { page } from '$app/state';
+
 	let { children } = $props();
+
+	let navigation_items = [
+		{ route: '/', label: 'About' },
+		{ route: '/projects', label: 'Portfolio' },
+		{ route: '/cv', label: 'CV' }
+	];
 </script>
 
-{@render children()}
+<!-- Header -->
+<div
+	class="fixed top-0 z-50 flex h-28 w-full items-center justify-between border-b px-4 backdrop-blur-md md:px-32"
+>
+	<!-- Title -->
+	<div>
+		<h1 class="text-4xl">Francisco Cunha</h1>
+		<h2 class="text-2xl">Developer, or some other title</h2>
+	</div>
+
+	<!-- Navigation -->
+	<div class="text-2xl">
+		{#each navigation_items as item, i}
+			<a
+				href={item.route}
+				class={{ 'font-semibold': page.url.pathname === item.route }}
+				aria-current={page.url.pathname === item.route}
+			>
+				{item.label}
+			</a>
+			{#if i + 1 != navigation_items.length}
+				<!-- pipe character followed by a space -->
+				|&nbsp;
+			{/if}
+		{/each}
+	</div>
+</div>
+
+<!-- Contents -->
+<div class="mx-4 my-32 md:mx-32">
+	{@render children()}
+</div>
