8e0d09d89389c8af51da92343177ee37c6ed9051
Francisco Cunha <francisccdn@gmail.com>
2025-01-04 17:19:34 +0100

    Improvements to project card layout

--- src/lib/project_card.svelte
+++ src/lib/project_card.svelte
@@ -3,6 +3,8 @@
 
 	// TODO get data programatically
 
+	let { src } = $props();
+
 	let title = 'Cloud Hopper';
 	let description =
 		"Jump amongst the clouds in a cute sky archipelago in this short (~20 min) 3D platformer. Find keys to progress and the optional jewels for an extra challenge. Can you reach the island at the top? Cloud Hopper was developed as a course assignment within the Game Studies minor at Leiden University. It's my very first 3D game and my first real effort in level design, but mainly it was a lot of fun to make! Hope you also have fun playing :)";
@@ -10,16 +12,16 @@
 </script>
 
 <div
-	class="flex h-[30rem] w-[26rem] gap-4 rounded-lg border-2 max-md:flex-col max-md:px-4 max-md:py-2 md:h-[16rem] md:w-[36rem] md:pr-4"
+	class="flex h-[30rem] w-[26rem] gap-4 rounded-lg border-2 max-md:flex-col max-md:pb-2 md:h-[16rem] md:w-[36rem] md:pr-4 lg:w-[34vw]"
 >
 	<!-- Left side: Image -->
 	<img
 		alt={'Image showcasing ' + title}
-		src="https://img.itch.zone/aW1nLzE5MTY1MjQ1LnBuZw==/315x250%23c/YfVUJ%2F.png"
-		class="max-md:h-1/2 max-md:w-4/5 max-md:self-center"
+		{src}
+		class="overflow-hidden object-cover max-md:h-1/2 max-md:w-full max-md:self-center max-md:rounded-t-md md:w-3/5 md:rounded-l-md"
 	/>
 	<!-- Right side: Details -->
-	<div class="flex flex-col justify-between gap-2 py-2 max-md:h-1/2 md:h-full">
+	<div class="flex flex-col justify-between gap-2 py-2 max-md:h-1/2 max-md:px-4 md:h-full md:w-2/5">
 		<!-- Title -->
 		<h2 class="text-xl font-semibold">{title}</h2>
 		<!-- Description -->
