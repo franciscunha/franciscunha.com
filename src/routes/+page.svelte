a9efedea9496965d6359bc7cf043061ef03cfed5
Francisco Cunha <francisccdn@gmail.com>
2025-01-07 15:12:18 +0100

    Remove placeholder content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,15 +42,8 @@
 		<div>
 			<h1>Hi! My name is Francisco Cunha</h1>
 			<p>
-				Voluptatibus distinctio officiis autem velit consequatur nihil explicabo provident. Laborum
-				sit ut aut sapiente fugiat odit. Tempora ipsum eum quis qui voluptatibus temporibus
-				blanditiis. Inventore ipsam officiis enim dolorem corporis dolores expedita. Pariatur
-				explicabo commodi earum ut id.
-			</p>
-			<p>
-				Et autem vel voluptatem et magnam. Omnis nihil et eligendi alias tempore dolore omnis aut.
-				Ad error dolores aliquid at nulla ipsam. Consequatur labore dolorem non aut nihil qui ab ut.
-				Atque reiciendis et quisquam ex in voluptatem nihil eius. Qui ipsa omnis aperiam dolorum.
+				This is my personal website. It's still under construction, so check back soon for an update
+				to this page and to see the projects I've worked on!
 			</p>
 		</div>
 
