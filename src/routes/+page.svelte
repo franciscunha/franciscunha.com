9d1031b9404f7d6ee9ecb8e541edf14a3420215f
Francisco Cunha <francisccdn@gmail.com>
2025-01-09 12:58:37 +0100

    Update about me contents

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import ItchioIcon from '$lib/icons/itchio_icon.svelte';
 	import LinkedinIcon from '$lib/icons/linkedin_icon.svelte';
 	import ProjectCard from '$lib/project_card.svelte';
+	import AboutMe from './about_me.md';
 
 	import { get_highlighted } from '$lib/load_project';
 	import { fly } from 'svelte/transition';
@@ -26,33 +27,38 @@
 	let project_id = $derived(highlighted_projects[project_index]);
 </script>
 
-<div class="flex w-full place-items-center gap-24 max-md:flex-col-reverse md:flex-row">
-	<div>
-		<img src="images/me.png" alt="Francisco Cunha" class="object-contain max-md:h-1/3" />
-		<h1 class="text-bold mb-2 mt-8">Find me at</h1>
-		<div class="flex flex-col items-start gap-2">
-			{#each socials as social}
-				<a href={social.href} class="flex flex-row items-center gap-2"
-					><social.icon size={2} />{social.label}</a
-				>
-			{/each}
+<div
+	class="flex w-full place-items-center gap-24 max-md:flex-col-reverse md:flex-row md:justify-center"
+>
+	<div class="max-md:flex max-md:flex-col-reverse">
+		<img
+			src="images/me.png"
+			alt="Francisco Cunha's face"
+			class="max-h-[50vh] object-contain max-md:h-1/3"
+		/>
+		<div class="max-md:-mt-32 max-md:mb-8 md:mt-8">
+			<h1 class="text-bold mb-2">Find me at</h1>
+			<div class="flex flex-col items-start gap-2">
+				{#each socials as social}
+					<a href={social.href} class="flex flex-row items-center gap-2"
+						><social.icon size={2} />{social.label}</a
+					>
+				{/each}
+			</div>
 		</div>
 	</div>
-	<div>
+	<div class="max-w-2xl max-md:flex max-md:flex-col">
 		<div>
-			<h1>Hi! My name is Francisco Cunha</h1>
-			<p>
-				This is my personal website. It's still under construction, so check back soon for an update
-				to this page and to see the projects I've worked on!
-			</p>
+			<h1>About me</h1>
+			<AboutMe />
 		</div>
 
-		<h2 class="my-8 text-2xl">
+		<h2 class="my-8 mt-14">
 			<span class="font-semibold">Highlighted projects</span> |
-			<a href="/portfolio">All projects</a>
+			<a href="/portfolio" class="underline">All projects</a>
 		</h2>
 
-		<div class="w-max">
+		<div class="w-max max-md:self-center">
 			{#key project_id}
 				<div in:fly={{ x: prev_p_index < project_index ? 100 : -100, duration: 250 }}>
 					<ProjectCard id={project_id} />
@@ -84,8 +90,7 @@
 		@apply font-semibold;
 	}
 
-	p {
-		@apply mt-4;
-		@apply text-lg;
+	h2 {
+		@apply text-2xl;
 	}
 </style>
