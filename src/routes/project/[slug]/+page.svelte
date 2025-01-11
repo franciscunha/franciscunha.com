e89ff5f70ec2f6ca9a6f3b81c6dab220f0b12313
Francisco Cunha <francisccdn@gmail.com>
2025-01-11 17:52:01 +0100

    Update project page layout and styles

--- src/routes/project/[slug]/+page.svelte
+++ src/routes/project/[slug]/+page.svelte
@@ -2,6 +2,7 @@
 	import Tag from '$lib/tag.svelte';
 
 	let { data } = $props();
+	let has_sidebar = data.meta.sidebar && data.meta.sidebar.length > 0;
 </script>
 
 <div class={'border-bordercolor md:rounded-lg md:border-2 md:p-8'}>
@@ -14,13 +15,14 @@
 			<p class="ml-4 text-lg font-semibold">{data.meta.year}</p>
 		</div>
 	</div>
+
 	<div class="flex gap-8 max-lg:flex-col lg:flex-row">
-		<div class="project-wrapper flex flex-col gap-4">
+		<div class={['project-wrapper flex flex-col gap-4', has_sidebar && 'md:w-2/3']}>
 			<data.content class="project-wrapper" />
 		</div>
 
-		{#if data.meta.sidebar && data.meta.sidebar.length > 0}
-			<div class="flex flex-col gap-8">
+		{#if has_sidebar}
+			<div class="flex flex-col gap-8 md:w-1/3">
 				{#each data.meta.sidebar as src, i}
 					<img {src} alt={'Showcase of ' + data.meta.name + ' #' + i} />
 				{/each}
@@ -56,5 +58,16 @@
 	/* Images will always have a parent <p>, so this are styles for the image wrapper */
 	.project-wrapper :global(p:has(img)) {
 		@apply self-center;
+	}
+
+	/* Code blocks */
+	.project-wrapper :global(pre) {
+		@apply rounded-lg;
+		@apply bg-bordercolor;
+		@apply p-4;
+	}
+
+	.project-wrapper :global(pre > code) {
+		@apply text-wrap;
 	}
 </style>
